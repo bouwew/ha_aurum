c2e68074fcc3fec0acaa294b37fae6a90c79111f
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-08-24 12:45:55 +0200

    Solve blocking in the IO-loop

--- custom_components/ha_aurum/__init__.py
+++ custom_components/ha_aurum/__init__.py
@@ -111,10 +111,7 @@ async def async_setup_entry(hass, entry):
 
     entry.add_update_listener(_update_listener)
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
